cc02db8e6b92e4dd563f1845918032f331f94b67
Antreev Singh Brar <antreevsinghbrar@Antreevs-MacBook-Pro.local>
2020-05-23 14:22:32 +0530

    initial try

---
+++ yolov3/main.py
@@ -0,0 +1,118 @@
+
+import matplotlib.pyplot as plt
+import  cv2
+%matplotlib inline
+from model import *
+from utils import *
+import os
+import time
+import logging
+import argparse
+import numpy as np
+import random
+
+
+model = make_yolov3_model()
+
+# load the model weights
+# I have loaded the pretrained weights in a separate dataset
+weight_reader = WeightReader('yolov3.weights')
+
+# set the model weights into the model
+weight_reader.load_weights(model)
+
+# save the model to file
+model.save('model.h5')
+
+from keras.models import load_model
+model = load_model('model.h5')
+
+model.summary()
+
+anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
+
+# define the expected input shape for the model
+WIDTH, HEIGHT = 416, 416
+
+# define the probability threshold for detected objects
+class_threshold = 0.6
+
+import os
+from matplotlib import pyplot as plt
+images=os.listdir('drive/My Drive/images')
+
+from numpy import expand_dims
+from keras.preprocessing.image import load_img, img_to_array
+
+# load and prepare an image
+def load_image_pixels(filename, shape):
+    '''
+    Function preprocess the images to 416x416, which is the standard input shape for YOLOv3, 
+    and also keeps track of the originl shape, which is later used to draw the boxes.
+    
+    paramters:
+    filename {String}: path to the image
+    shape {tuple}: shape of the input dimensions of the network
+    
+    returns:
+    image {PIL}: image of shape 'shape'
+    width {int}: original width of the picture
+    height {int}: original height of the picture
+    '''
+    # load the image to get its shape
+    image = load_img(filename)
+    width, height = image.size
+    
+    # load the image with the required size
+    image = load_img(filename, target_size=shape)
+    
+    # convert to numpy array
+    image = img_to_array(image)
+    
+    # scale pixel values to [0, 1]
+    image = image.astype('float32')
+    image /= 255.0
+    
+    # add a dimension so that we have one sample
+    image = expand_dims(image, 0)
+    return image, width, height
+
+import tensorflow as tf
+
+for file in images:
+    photo_filename ='drive/My Drive/images/' + file
+    #a = tf.timestamp()
+    # load picture with old dimensions
+    image, image_w, image_h = load_image_pixels(photo_filename, (WIDTH, HEIGHT))
+    
+    # Predict image
+    yhat = model.predict(image)
+    print(len(yhat))
+
+    # Create boxes
+    boxes = list()
+    for i in range(len(yhat)):
+        # decode the output of the network
+        boxes += decode_netout(yhat[i][0], anchors[i], class_threshold, HEIGHT, WIDTH)
+
+    # correct the sizes of the bounding boxes for the shape of the image
+    correct_yolo_boxes(boxes, image_h, image_w, HEIGHT, WIDTH)
+
+    # suppress non-maximal boxes
+    do_nms(boxes, 0.5)
+
+    # define the labels (Filtered only the ones relevant for this task, which were used in pretraining the YOLOv3 model)
+    labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck","boat"]
+
+    # get the details of the detected objects
+    v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
+
+    # summarize what we found
+    for i in range(len(v_boxes)):
+
+        print(v_labels[i], v_scores[i])
+
+    # draw what we found
+    draw_boxes(photo_filename, v_boxes, v_labels, v_scores)
+    #b= tf.timestamp()
+    #print(b-a)
